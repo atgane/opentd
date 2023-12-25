8661afd4349b77ff9245a17a211cc288e01c10c5
atgane <hyper201286@gmail.com>
2023-12-25 16:04:59 +0900

    add event branch

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -6,30 +6,47 @@ import (
 	"net"
 
 	"github.com/atgane/opentd/apis"
+	"github.com/atgane/opentd/pkgs/events"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 )
 
 type config struct {
-	GRPCPort int
+	GRPCPort   int
+	EventType  string
+	NATSConfig events.NATSConfig
+	LogLevel   string
 }
 
 func main() {
 	// TODO: make env loader
 	conf := config{
-		GRPCPort: 17011,
+		GRPCPort:  17011,
+		EventType: "",
+	}
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	if conf.EventType == events.NATS {
+	} else {
+		log.Fatal().Err(fmt.Errorf("unidentified events"))
+		return
 	}
 
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", conf.GRPCPort))
 	if err != nil {
 		log.Fatal().Err(err)
+		return
 	}
 	gs, err := newFrontend(conf)
 	if err != nil {
 		log.Fatal().Err(err)
+		return
 	}
 	if err := gs.Serve(l); err != nil {
 		log.Fatal().Err(err)
+		return
 	}
 }
 
--- pkgs/events/event.go
+++ pkgs/events/event.go
@@ -2,4 +2,8 @@ package events
 
 import cloudevents "github.com/cloudevents/sdk-go/v2"
 
+const (
+	NATS = "nats"
+)
+
 type NewEventClient[T any] func(T) (cloudevents.Client, error)
