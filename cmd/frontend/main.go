141b566ca55fd679b9db59c73a0e7fe9c7fbd552
atgane <hyper201286@gmail.com>
2023-12-25 12:32:41 +0900

    add module

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -1,8 +1,63 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/atgane/opentd/apis"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+)
+
 type config struct {
 	GRPCPort int
 }
 
 func main() {
+	// TODO: make env loader
+	conf := config{
+		GRPCPort: 17011,
+	}
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", conf.GRPCPort))
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	gs, err := newFrontend(conf)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	if err := gs.Serve(l); err != nil {
+		log.Fatal().Err(err)
+	}
+}
+
+type frontend struct {
+	apis.UnimplementedFrontendServer
+}
+
+func newFrontend(c config) (*grpc.Server, error) {
+	gs := grpc.NewServer()
+	fs := new(frontend)
+	apis.RegisterFrontendServer(gs, fs)
+	return gs, nil
+}
+
+func (f *frontend) Buy(ctx context.Context, req *apis.BuyRequest) (*apis.BuyResponse, error) {
+	// TODO: add otel tracing
+	// TODO: impl logic
+	return nil, nil
+}
+
+func (f *frontend) Sell(ctx context.Context, req *apis.SellRequest) (*apis.SellResponse, error) {
+	return nil, nil
+}
+
+func (f *frontend) Cancel(ctx context.Context, req *apis.CancelRequest) (*apis.CancelResponse, error) {
+	return nil, nil
+}
+
+func (f *frontend) Update(ctx context.Context, req *apis.UpdateRequest) (*apis.UpdateResponse, error) {
+	return nil, nil
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,23 @@ require (
 )
 
 require (
+	github.com/cloudevents/sdk-go/protocol/nats/v2 v2.14.0 // indirect
+	github.com/cloudevents/sdk-go/v2 v2.14.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/nats-io/nats.go v1.13.1-0.20220308171302-2f2f6968e98d // indirect
+	github.com/nats-io/nkeys v0.3.0 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/rs/zerolog v1.31.0 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
