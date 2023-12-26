25782c45ed7cf75ecdef4baa9011401971e96bb2
atgane <hyper201286@gmail.com>
2023-12-27 01:43:07 +0900

    impl cancel logic

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -14,18 +14,21 @@ import (
 	"github.com/redis/go-redis/v9"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-type config struct {
-	GRPCPort    int
-	EventConfig events.EventConfig
-	RedisConfig redis.Options
-	LogLevel    string
+type FrontConfig struct {
+	GRPCPort         int
+	EventConfig      events.EventConfig
+	RedisConfig      redis.Options
+	LogLevel         string
+	LockExpireSecond time.Duration
 }
 
 func main() {
 	// TODO: make env loader
-	conf := config{
+	conf := FrontConfig{
 		GRPCPort: 17011,
 		EventConfig: events.EventConfig{
 			EventType: events.NATS,
@@ -37,7 +40,8 @@ func main() {
 		RedisConfig: redis.Options{
 			Addr: "localhost:6379",
 		},
-		LogLevel: "trace",
+		LogLevel:         "trace",
+		LockExpireSecond: 300 * time.Second,
 	}
 
 	logging.SetLevel(conf.LogLevel)
@@ -54,15 +58,16 @@ func main() {
 }
 
 type frontend struct {
-	producerClient cloudevents.Client
-	redisClient    *redis.Client
-	port           int
-	gs             *grpc.Server
+	producerClient   cloudevents.Client
+	redisClient      *redis.Client
+	port             int
+	lockExpireSecond time.Duration
+	gs               *grpc.Server
 
 	apis.UnimplementedFrontendServer
 }
 
-func NewFrontend(conf config) (*frontend, error) {
+func NewFrontend(conf FrontConfig) (*frontend, error) {
 	ctx := context.Background()
 
 	producerClient, err := events.NewProducerEvent(conf.EventConfig)
@@ -81,6 +86,7 @@ func NewFrontend(conf config) (*frontend, error) {
 	fs.producerClient = producerClient
 	fs.redisClient = redisClient
 	fs.port = conf.GRPCPort
+	fs.lockExpireSecond = conf.LockExpireSecond
 	fs.gs = gs
 	apis.RegisterFrontendServer(gs, fs)
 	return fs, nil
@@ -157,8 +163,51 @@ func (f *frontend) Sell(ctx context.Context, req *apis.SellRequest) (*apis.SellR
 }
 
 func (f *frontend) Cancel(ctx context.Context, req *apis.CancelRequest) (*apis.CancelResponse, error) {
-	// TODO: impl logic
-	return nil, nil
+	log.Debug().Interface("req", req).Msg("cancel order accepted")
+
+	result := f.redisClient.SetNX(ctx, req.RequestId, 1, f.lockExpireSecond)
+	success, err := result.Result()
+	if err != nil {
+		log.Error().
+			Err(err).
+			Str("user_id", req.UserId).
+			Str("request_id", req.RequestId).
+			Msg("failed to f.redisClient.SetNX()")
+		return nil, err
+	}
+	if !success {
+		msg := "deal already has been executed"
+		log.Error().
+			Err(err).
+			Str("user_id", req.UserId).
+			Str("request_id", req.RequestId).
+			Msg("msg")
+		return nil, status.Errorf(codes.AlreadyExists, msg)
+	}
+
+	rid := uuid.New()
+
+	e := cloudevents.NewEvent()
+	e.SetID(rid.String())
+	e.SetType(events.SellType)
+	e.SetTime(time.Now())
+	e.SetSource(events.FrontendSource)
+	_ = e.SetData(cloudevents.ApplicationJSON, req)
+
+	if result := f.producerClient.Send(ctx, e); cloudevents.IsUndelivered(result) {
+		err := fmt.Errorf("cloud event message send failed")
+		log.Error().
+			Err(err).
+			Str("user_id", req.UserId).
+			Str("target", req.RequestId).
+			Msg("failed to f.producerClient.Send()")
+		return nil, err
+	}
+
+	res := new(apis.CancelResponse)
+	res.RequestId = rid.String()
+
+	return res, nil
 }
 
 func (f *frontend) Update(ctx context.Context, req *apis.UpdateRequest) (*apis.UpdateResponse, error) {
