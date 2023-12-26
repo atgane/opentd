a698e30f1af9141faa938f88c5d1d6601fec8df0
atgane <hyper201286@gmail.com>
2023-12-26 11:55:17 +0900

    add cloud event spec

---
+++ pkgs/events/spec.go
@@ -0,0 +1,13 @@
+package events
+
+const (
+	FrontendSource = "opentd/frontend"
+)
+
+const (
+	BuyType    = "com.atgane.opentd.Buy.Order"
+	SellType   = "com.atgane.opentd.Sell.Order"
+	CancelType = "com.atgane.opentd.Cancel.Order"
+	UpdateType = "com.atgane.opentd.Update.Order"
+	DealType   = "com.atgane.opentd.Stream.Deal"
+)
